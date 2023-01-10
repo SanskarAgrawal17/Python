bea34d65c1b80d936dd979eef8cb36b2cec47164
Sanskar Agrawal <104454636+geekysans@users.noreply.github.com>
2023-01-10 18:29:23 +0530

    Add files via upload

---
+++ Armstrong_Number.py
@@ -0,0 +1,11 @@
+n = int(input("Enter The Number: "))
+n1 = n
+sum = 0
+while n != 0:
+    r = n % 10
+    sum = sum + (r**len(str(n1)))
+    n = int(n/10)
+if n1 == sum:
+    print("Number is armstrong number : ")
+else:
+    print("Number is not armstrong number :")
